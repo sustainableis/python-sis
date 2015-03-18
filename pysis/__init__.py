6301fc341e113f039c571f52d4c9d1a54ea6aed3
Chet Helms <chetwhelms@gmail.com>
2015-03-18 00:41:54 -0400

    Version 0.3
    v0.3 added oauth refreshToken support.

--- pysis/__init__.py
+++ pysis/__init__.py
@@ -1,10 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 __title__ = 'pysis'
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = 'Chet Helms'
 __email__ = 'chetwhelms@gmail.com'
 __license__ = ''
-__copyright__ = 'Copyright 2014 Chet Helms'
+__copyright__ = 'Copyright 2015 Chet Helms'
 
 from .sis import SIS
