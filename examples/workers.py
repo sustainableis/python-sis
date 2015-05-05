18cf1242179534e30da57836539a771e248692c5
John Crawford <jrcrawfo@gmail.com>
2015-05-05 09:09:35 -0400

    Example of workers

---
+++ examples/workers.py
@@ -0,0 +1,17 @@
+from pysis import SIS
+
+s = SIS(token="4eeb61e2cac55f77e6402df50e105b1923255a79")
+
+print("\nConfigurations (All) \n---------")
+
+worker = s.workers.get(uuid="d91adc8c-8961-907d-4478-019da1b2d483")
+print worker.uuid, worker.label
+
+configs = worker.getConfigurations()
+for config in configs:
+  print config.environment,config.title,config.description
+
+prod_configs = worker.getConfigurations(environment='development')
+for config in prod_configs:
+  print 'DEV: %s %s'%(config.environment, config.title)
+
