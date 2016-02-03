ad74df9112719fa1d7ae9e4708b54f97733e88af
Jeremy <jeremy.freeman@raviolilabs.com>
2016-02-03 16:08:18 -0500

    stub for reports

---
+++ pysis/services/reports/__init__.py
@@ -0,0 +1,70 @@
+from pysis.services.base import Service
+from uuid import UUID
+
+class Reports(Service):
+    """Reports Service
+
+    Consumes Reports API: <{url}/reports>
+    """
+    def __init__(self, client):
+        """Creates Emails object with a client"""
+        super(Reports, self).__init__(client)
+
+    '''
+    #below are prototype functions shamelessly stolen from the emails services for copy...  I mean inspiration
+    def getSubscriptions(self, id=None, type=None):
+        """Gets Email Subscriptions from the API
+
+        Args:
+            id (string): id of the email subscription.
+                if None, returns all email_subscriptions
+            type (string): type of the email_subscription
+
+
+        Returns: email subscription resources
+        """
+        if id is None:
+
+            params = {}
+            if type:
+                params['type'] = type
+
+            request = self.request_builder('emails.getSubscriptions', **params)
+
+        else:
+
+            # try-parse UUID
+            try:
+                u = UUID(id);
+            except ValueError:
+
+                print('id must be a valid UUID')
+
+                return
+
+            request = self.request_builder('emails.getSubscriptions', id=id)
+
+        return self._get(request)
+
+    def createSubscription(self, subscription):
+
+        request = self.request_builder('emails.createSubscription', body=subscription)
+
+        return self._post(request)
+
+
+    def triggerSubscription(self, id, data):
+
+        # try-parse UUID
+        try:
+            u = UUID(id);
+        except ValueError:
+
+            print('id must be a valid UUID')
+
+            return
+
+        request = self.request_builder('emails.triggerSubscription', id=id, body=data)
+
+        return self._post(request)
+    '''
