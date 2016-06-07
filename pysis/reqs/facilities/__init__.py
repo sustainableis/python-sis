e9e77858097d25b97c27027f7edc4bfdef792ae8
evancasey1 <evan.casey@certirx.com>
2016-06-07 11:59:25 -0400

    Added new pysis-api with utilities support

--- pysis/reqs/facilities/__init__.py
+++ pysis/reqs/facilities/__init__.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 from pysis.reqs.base import Request
+from pysis.resources.utilities import UtilitySummary
 from pysis.resources.facilities import Facilities
 from pysis.resources.facilities import FacilityInfo
 from pysis.resources.buildings import Buildings
@@ -8,7 +9,6 @@ from pysis.resources.users import Users
 from pysis.resources.feeds import Feeds
 from pysis.resources.outputs import Outputs
 from pysis.resources.blastcells import Blastcells
-from pysis.resources.utilities import UtilitySummary
 from pysis.resources.utilities import UtilityMeters
 from pysis.resources.utilities import UtilityStatements
 from pysis.resources.outputs import OutputsTree
@@ -126,3 +126,4 @@ class GetInfo(Request):
     def clean_uri(self):
 
         return 'facilities/{id}/info'
+
