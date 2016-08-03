2d4989848b0e3b77594e5f6f87eea9b61b7349ad
evancasey1 <evan.casey@certirx.com>
2016-08-03 13:51:17 -0400

    removed test code

--- pysis/sis.py
+++ pysis/sis.py
@@ -4,10 +4,10 @@ from datetime import datetime
 import calendar
 
 class SIS(object):
-    #__BASE_URL__ = 'http://api.sustainableis.com/v1/'
-    #__API_DOMAIN__ = 'api.sustainableis.com'
-    __BASE_URL__ = 'http://localhost:3000/v1/'
-    __API_DOMAIN__ = 'localhost:3000'
+    __BASE_URL__ = 'http://api.sustainableis.com/v1/'
+    __API_DOMAIN__ = 'api.sustainableis.com'
+    #__BASE_URL__ = 'http://localhost:3000/v1/'
+    #__API_DOMAIN__ = 'localhost:3000'
 
 
 
